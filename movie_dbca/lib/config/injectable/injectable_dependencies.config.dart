5fa1b2ead0d2515f4a274da9b0d6986c392574d0
samu7861 <sfrancog7861@gmail.com>
2024-10-05 19:59:11 -0500

    useCase

--- movie_dbca/lib/config/injectable/injectable_dependencies.config.dart
+++ movie_dbca/lib/config/injectable/injectable_dependencies.config.dart
@@ -11,17 +11,28 @@
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 
+import '../../modules/home/domain/repositories/movie_repository.dart' as _i946;
+import '../../modules/home/domain/usecases/movies_usecase.dart' as _i1027;
+
 extension GetItInjectableX on _i174.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
   _i174.GetIt init({
     String? environment,
     _i526.EnvironmentFilter? environmentFilter,
   }) {
-    _i526.GetItHelper(
+    final gh = _i526.GetItHelper(
       this,
       environment,
       environmentFilter,
     );
+    gh.factory<_i1027.GetNowPlayingUsecase>(() => _i1027.GetNowPlayingUsecase(
+        iMovieRepository: gh<_i946.IMovieRepository>()));
+    gh.factory<_i1027.GetUpcomingUsecase>(() => _i1027.GetUpcomingUsecase(
+        iMovieRepository: gh<_i946.IMovieRepository>()));
+    gh.factory<_i1027.GetTopRatedUsecase>(() => _i1027.GetTopRatedUsecase(
+        iMovieRepository: gh<_i946.IMovieRepository>()));
+    gh.factory<_i1027.SearchMovieUsecase>(() => _i1027.SearchMovieUsecase(
+        iMovieRepository: gh<_i946.IMovieRepository>()));
     return this;
   }
 }
--- movie_dbca/lib/modules/home/domain/usecases/movies_usecase.dart
+++ movie_dbca/lib/modules/home/domain/usecases/movies_usecase.dart
@@ -1,6 +1,62 @@
+import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
+import 'package:movie_dbca/core/error/error.dart';
+import 'package:movie_dbca/core/usecase/usecase.dart';
+import 'package:movie_dbca/modules/home/domain/entities/movie_entity.dart';
+import 'package:movie_dbca/modules/home/domain/repositories/movie_repository.dart';
 
 @Injectable()
-class MoviesUsecase {
-  
+class GetNowPlayingUsecase extends UseCase<List<MovieEntity>, String> {
+  final IMovieRepository _iMovieRepository;
+
+  GetNowPlayingUsecase({
+    required IMovieRepository iMovieRepository,
+  }) : _iMovieRepository = iMovieRepository;
+
+  @override
+  Future<Either<Failure, List<MovieEntity>>> call(String params) async {
+    return await _iMovieRepository.getNowPlaying();
+  }
+}
+
+@Injectable()
+class GetUpcomingUsecase extends UseCase<List<MovieEntity>, String> {
+  final IMovieRepository _iMovieRepository;
+
+  GetUpcomingUsecase({
+    required IMovieRepository iMovieRepository,
+  }) : _iMovieRepository = iMovieRepository;
+
+  @override
+  Future<Either<Failure, List<MovieEntity>>> call(String params) async {
+    return await _iMovieRepository.getUpcoming();
+  }
+}
+
+@Injectable()
+class GetTopRatedUsecase extends UseCase<List<MovieEntity>, String> {
+  final IMovieRepository _iMovieRepository;
+
+  GetTopRatedUsecase({
+    required IMovieRepository iMovieRepository,
+  }) : _iMovieRepository = iMovieRepository;
+
+  @override
+  Future<Either<Failure, List<MovieEntity>>> call(String params) async {
+    return await _iMovieRepository.getTopRated();
+  }
+}
+
+@Injectable()
+class SearchMovieUsecase extends UseCase<List<MovieEntity>, String> {
+  final IMovieRepository _iMovieRepository;
+
+  SearchMovieUsecase({
+    required IMovieRepository iMovieRepository,
+  }) : _iMovieRepository = iMovieRepository;
+
+  @override
+  Future<Either<Failure, List<MovieEntity>>> call(String params) async {
+    return await _iMovieRepository.searchMovie();
+  }
 }
